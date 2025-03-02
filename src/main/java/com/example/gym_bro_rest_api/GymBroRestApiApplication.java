ec5c6b1c4e0281ade8645753a7b11522cab0a3fc
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-03-02 17:10:45 +0100

    spring boot project folder

---
+++ src/main/java/com/example/gym_bro_rest_api/GymBroRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gym_bro_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBroRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBroRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gym_bro_rest_api/GymBroRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gym_bro_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBroRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
