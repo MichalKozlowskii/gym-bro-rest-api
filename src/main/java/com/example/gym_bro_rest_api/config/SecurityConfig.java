c36414aeb5f7ac08af8d69d319722c29424031aa
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-03-02 20:15:16 +0100

    feat: create custom user details service and put it in configuration

--- src/main/java/com/example/gym_bro_rest_api/config/SecurityConfig.java
+++ src/main/java/com/example/gym_bro_rest_api/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.example.gym_bro_rest_api.config;
 
+import com.example.gym_bro_rest_api.mappers.UserMapper;
+import com.example.gym_bro_rest_api.repositories.UserRepository;
+import com.example.gym_bro_rest_api.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository, UserMapper userMapper) {
+        return new CustomUserDetailsService(userRepository);
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
