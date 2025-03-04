aae26c499db04d3dce32f2b4c0135204a1f0a326
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-03-04 22:01:55 +0100

    feat: user service find user by username

--- src/main/java/com/example/gym_bro_rest_api/services/UserService.java
+++ src/main/java/com/example/gym_bro_rest_api/services/UserService.java
@@ -2,6 +2,9 @@ package com.example.gym_bro_rest_api.services;
 
 import com.example.gym_bro_rest_api.model.UserDTO;
 
+import java.util.Optional;
+
 public interface UserService {
     UserDTO saveNewUser(UserDTO userDTO);
+    Optional<UserDTO> findUserByUsername(String username);
 }
--- src/main/java/com/example/gym_bro_rest_api/services/UserServiceImpl.java
+++ src/main/java/com/example/gym_bro_rest_api/services/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.gym_bro_rest_api.services;
 
+import com.example.gym_bro_rest_api.entities.User;
 import com.example.gym_bro_rest_api.model.UserDTO;
 import com.example.gym_bro_rest_api.mappers.UserMapper;
 import com.example.gym_bro_rest_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +17,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO saveNewUser(UserDTO userDTO) {
         return userMapper.UserToUserDto(userRepository.save(userMapper.UserDtoToUser(userDTO)));
+    }
+
+    @Override
+    public Optional<UserDTO> findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(userMapper::UserToUserDto);
     }
 }
