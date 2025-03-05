d7a4a6a339b50ac86f1674e1d969341d873bb112
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-03-05 12:25:18 +0100

    refactor: login post

--- src/main/java/com/example/gym_bro_rest_api/controller/AuthController.java
+++ src/main/java/com/example/gym_bro_rest_api/controller/AuthController.java
@@ -4,17 +4,21 @@ import com.example.gym_bro_rest_api.entities.User;
 import com.example.gym_bro_rest_api.model.UserDTO;
 import com.example.gym_bro_rest_api.services.JwtService;
 import com.example.gym_bro_rest_api.services.OAuth2UserService;
+import com.example.gym_bro_rest_api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Map;
 
@@ -25,19 +29,27 @@ import java.util.Map;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    private final UserService userService;
     private final OAuth2UserService oAuth2UserService;
-    
+
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody UserDTO userDTO) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDTO.getUsername(),
-                userDTO.getPassword()));
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    userDTO.getUsername(), userDTO.getPassword()));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("error", "Invalid username or password."));
+        }
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(userDTO.getUsername());
-        String token = jwtService.generateToken(userDetails.getUsername());
-
-        return ResponseEntity.ok(Map.of("token", token));
+        String token = jwtService.generateToken(userDTO.getUsername());
+        return ResponseEntity.ok(Map.of("jwt_token", token));
     }
+
+    /*@PostMapping("/register")
+    public ResponseEntity<Map<String, String>> register(@RequestBody UserDTO userDTO) {
+
+    }*/
 
     @GetMapping("/oauth-success")
     public ResponseEntity<Map<String, String>> oAuthSuccess(@RequestParam("token") String token) {
