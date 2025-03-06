f479d2717b512b523cf1c91c53b1e8be2ff9d641
Michał <michal@saturdaysheroes.dev>
2025-03-06 17:09:42 +0100

    fix: exercise mapper userid mapping

--- src/main/java/com/example/gym_bro_rest_api/mappers/ExerciseMapper.java
+++ src/main/java/com/example/gym_bro_rest_api/mappers/ExerciseMapper.java
@@ -3,9 +3,13 @@ package com.example.gym_bro_rest_api.mappers;
 import com.example.gym_bro_rest_api.entities.Exercise;
 import com.example.gym_bro_rest_api.model.ExerciseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface ExerciseMapper {
+
+    @Mapping(source = "user.id", target = "userId")
     ExerciseDTO exerciseToExerciseDto(Exercise exercise);
+    @Mapping(source = "userId", target = "user.id")
     Exercise exerciseDtoToExercise(ExerciseDTO exerciseDTO);
 }
