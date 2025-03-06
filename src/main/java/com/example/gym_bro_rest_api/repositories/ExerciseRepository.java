1821c32b3f4bcee76d41aec4fc282531181cdccb
Michał <michal@saturdaysheroes.dev>
2025-03-06 17:30:35 +0100

    feat: exercise repository

---
+++ src/main/java/com/example/gym_bro_rest_api/repositories/ExerciseRepository.java
@@ -0,0 +1,11 @@
+package com.example.gym_bro_rest_api.repositories;
+
+import com.example.gym_bro_rest_api.entities.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    List<Exercise> findExercisesByUserId(Long id);
+}
