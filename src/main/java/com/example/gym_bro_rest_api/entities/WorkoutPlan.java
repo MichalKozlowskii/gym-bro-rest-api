595f6b055a7c61c4e8a38b2fcc4c2c7f333b97bd
Michał <michal@saturdaysheroes.dev>
2025-03-09 01:37:42 +0100

    feat: workout plan entity and DTO

---
+++ src/main/java/com/example/gym_bro_rest_api/entities/WorkoutPlan.java
@@ -0,0 +1,40 @@
+package com.example.gym_bro_rest_api.entities;
+
+import com.example.gym_bro_rest_api.model.SetsReps;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "workout_plans")
+public class WorkoutPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    private String name;
+
+    @ManyToMany
+    private List<Exercise> exercises;
+
+    @ManyToOne
+    @NotNull
+    private User user;
+
+    @ElementCollection
+    @MapKeyJoinColumn(name = "exercise_id")
+    @Column(name = "sets_reps")
+    private Map<Exercise, SetsReps> exerciseDetails = new HashMap<>();
+}
---
+++ src/main/java/com/example/gym_bro_rest_api/model/WorkoutPlanDTO.java
@@ -0,0 +1,16 @@
+package com.example.gym_bro_rest_api.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+public class WorkoutPlanDTO {
+    private Long id;
+    private String name;
+    private Long userId;
+    private List<ExerciseDTO> exercises;
+    private List<SetsReps> exerciseDetails;
+}
