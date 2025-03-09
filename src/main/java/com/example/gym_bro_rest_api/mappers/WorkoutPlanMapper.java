f0a9b9d6967bf93f05fdfaf32e3d578973f6eca1
Michał <michal@saturdaysheroes.dev>
2025-03-09 19:18:01 +0100

    feat: workout plan mapper

---
+++ src/main/java/com/example/gym_bro_rest_api/mappers/WorkoutPlanMapper.java
@@ -0,0 +1,12 @@
+package com.example.gym_bro_rest_api.mappers;
+
+import com.example.gym_bro_rest_api.entities.WorkoutPlan;
+import com.example.gym_bro_rest_api.model.WorkoutPlanDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface WorkoutPlanMapper {
+    @Mapping(source = "user.id", target = "userId")
+    WorkoutPlanDTO workoutPlanToWorkoutPlanDto(WorkoutPlan workoutPlan);
+}
