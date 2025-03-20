5ed79bbbe2805d53322e604bd840bf56b5424d90
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-03-20 21:33:14 +0100

    feat: workoutplan query service

---
+++ src/main/java/com/example/gym_bro_rest_api/services/workoutplan/WorkoutPlanQueryServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.gym_bro_rest_api.services.workoutplan;
+
+import com.example.gym_bro_rest_api.controller.NotFoundException;
+import com.example.gym_bro_rest_api.entities.WorkoutPlan;
+import com.example.gym_bro_rest_api.repositories.WorkoutPlanrepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WorkoutPlanQueryServiceImpl implements WorkoutPlanQueryService {
+    private final WorkoutPlanrepository workoutPlanrepository;
+    @Override
+    public WorkoutPlan getWorkoutPlanById(Long id) {
+        return workoutPlanrepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+}
