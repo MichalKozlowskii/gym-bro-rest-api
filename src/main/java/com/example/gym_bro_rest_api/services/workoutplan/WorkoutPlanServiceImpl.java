413ebd595eb30bf238414621a80fbc2b910f1040
Michał <michal@saturdaysheroes.dev>
2025-03-30 00:53:17 +0100

    refactor: pagination utils

--- src/main/java/com/example/gym_bro_rest_api/services/workoutplan/WorkoutPlanServiceImpl.java
+++ src/main/java/com/example/gym_bro_rest_api/services/workoutplan/WorkoutPlanServiceImpl.java
@@ -4,14 +4,13 @@ import com.example.gym_bro_rest_api.controller.NoAccessException;
 import com.example.gym_bro_rest_api.controller.NotFoundException;
 import com.example.gym_bro_rest_api.entities.Exercise;
 import com.example.gym_bro_rest_api.entities.User;
-import com.example.gym_bro_rest_api.entities.Workout;
 import com.example.gym_bro_rest_api.entities.WorkoutPlan;
 import com.example.gym_bro_rest_api.mappers.WorkoutPlanMapper;
 import com.example.gym_bro_rest_api.model.ExerciseDTO;
 import com.example.gym_bro_rest_api.model.WorkoutPlanDTO;
 import com.example.gym_bro_rest_api.repositories.WorkoutPlanrepository;
 import com.example.gym_bro_rest_api.services.exercise.ExerciseQueryService;
-import com.example.gym_bro_rest_api.services.exercise.ExerciseServiceImpl;
+import com.example.gym_bro_rest_api.services.utils.PaginationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -103,7 +102,7 @@ public class WorkoutPlanServiceImpl implements WorkoutPlanService {
 
     @Override
     public Page<WorkoutPlanDTO> listExercisesOfUser(User user, Integer pageNumber, Integer pageSize) {
-        PageRequest pageRequest = ExerciseServiceImpl.buildPageRequest(pageNumber, pageSize);
+        PageRequest pageRequest = PaginationUtils.buildPageRequest(pageNumber, pageSize);
         Page<WorkoutPlan> workoutPlansPage = workoutPlanrepository.findWorkoutPlansByUserId(user.getId(), pageRequest);
 
         return workoutPlansPage.map(workoutPlanMapper::workoutPlanToWorkoutPlanDto);
