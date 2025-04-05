1e4ea30a223613babb93af45ac0fa78e98fedd17
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-04-05 19:24:06 +0200

    feat: exercise set repository

--- src/main/java/com/example/gym_bro_rest_api/repositories/ExerciseSetRepository.java
+++ src/main/java/com/example/gym_bro_rest_api/repositories/ExerciseSetRepository.java
@@ -2,7 +2,24 @@ package com.example.gym_bro_rest_api.repositories;
 
 import com.example.gym_bro_rest_api.entities.ExerciseSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ExerciseSetRepository extends JpaRepository<ExerciseSet, Long> {
-
+    ExerciseSet findTopByExerciseIdOrderByRepsDesc(Long exerciseId);
+    ExerciseSet findTopByExerciseIdOrderByRepsAsc(Long exerciseId);
+    ExerciseSet findTopByExerciseIdOrderByWeightDesc(Long exerciseId);
+    ExerciseSet findTopByExerciseIdOrderByWeightAsc(Long exerciseId);
+    ExerciseSet findTopByExerciseIdOrderByCreationDateDesc(Long exerciseId);
+    ExerciseSet findTopByExerciseIdOrderByCreationDateAsc(Long exerciseId);
+    @Query("""
+    SELECT s FROM ExerciseSet s
+    WHERE s.exercise.id = :exerciseId
+    AND s.weight = (SELECT MAX(s2.weight) FROM ExerciseSet s2 WHERE s2.exercise.id = :exerciseId AND s2.creationDate = s.creationDate)
+    GROUP BY s.creationDate
+    ORDER BY s.creationDate ASC
+    """)
+    List<ExerciseSet> findBestSetPerDay(@Param("exerciseId") Long exerciseId);
 }
