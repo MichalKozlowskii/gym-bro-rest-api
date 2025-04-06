ef83c622c4408dd9e9d543d349f44fd9a0ff65e8
Michał Kozłowski <michal.kozlowski032@gmail.com>
2025-04-06 21:21:23 +0200

    refactor: use spring doc instead

--- src/main/java/com/example/gym_bro_rest_api/GymBroRestApiApplication.java
+++ src/main/java/com/example/gym_bro_rest_api/GymBroRestApiApplication.java
@@ -2,11 +2,8 @@ package com.example.gym_bro_rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class GymBroRestApiApplication {
 
 	public static void main(String[] args) {
